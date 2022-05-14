401b91947dde44c440d8800224b035ecda23b087
samad aderinto <Adesamad134@gmail.com>
2022-05-14 17:47:51 +0100

    edited the css  and js

--- todo.js
+++ todo.js
@@ -1,42 +1,70 @@
-const todosContainer = document.querySelector('.todos');
-const button = document.querySelector('.data--button');
-const input = document.querySelector('.data--input');
-const  todos = document.querySelectorAll('.todos--item');
-const cancel = document.querySelectorAll(".absolute")
-
-cancel.forEach(curr => {
-    curr.addEventListener("click", e => {
-        console.log(e.parentElement)
-    })
-})
-const AddEl = () => {
-    let val = input.value
-    const className = "todos--item"
-    if (val === "") {
-        return
-    } else {
-        console.log(todos.length)
-        
-            const ref = ` <span class="${className}">${val}
-                           <div class="absolute">&times<div>
-                         </span>`
-       todosContainer.insertAdjacentHTML('beforeend', ref); 
-       input.value = ""
-       input.focus()
-    
-       
-    }        
- }
-button.addEventListener('click', e => {
-    e.preventDefault()
-    AddEl()
-})
-document.addEventListener('keypress', e => {
-    if (e.keycode === 13 || e.which === 13) {
-      AddEl()
-    } 
-    console.log(cancel.length)
-})
+const todosListEl = document.querySelector('[data-todo-list]');
+const defaultItem = document.querySelector('[data-todo-item-def]');
+const todosItem = document.querySelectorAll('[data-todo-item]');
+const deleteItem = document.querySelectorAll('[data-todo-item-del]');
+const todosNote = document.querySelectorAll("[data-todo-item-note]")
+const defaultItemData = document.querySelector("[data-todo]")
 
 
-// i am done {bye bye ugwu}
+function updateInput(element) {
+    element.value = ""
+}
+document.addEventListener("keypress", e => {
+    let inputValue = defaultItemData.value
+    if ((e.keycode === 13 || e.which === 13) && inputValue != "") {
+
+
+        const Object = `
+            <div class="todo__item" data-todo-item>
+                <button>
+                    <img src="edit-solid.svg" />
+                </button>
+                <input placeholder="New task...." type="text" value="${inputValue}" onfocus="${focusItem()}" onchange="${focusItem()}" spellcheck = "false"></input>
+
+                 <div class="details">
+                    <div class="details-info--1">
+                        <p>Notes</p>
+                        <textarea maxlength="270" data-todo-item-note></textarea>
+                    </div>
+
+                    <div class="details-info--2">
+                        <div class="details-info--2__date">
+                            <div>
+                                <input type="radio" name="date" id="Today"></input>
+                                <label for="Today">
+                                    Today
+                                </label>
+                            </div>
+                            <div>
+                                <input type="radio" name="date" id="Tommorow"></input>
+                                <label for="Tommorow">
+                                    Tommorow
+                                </label>
+                            </div>
+                            <div>
+                                <input type="date" name="date" id="calender"></input>
+                                <label for="calender">
+                                    Set date
+                                </label>
+                            </div>
+                            
+                        </div>
+                        <button data-todo-item-del>Delete</button>
+                        
+                        
+                    </div>
+
+                </div> 
+
+            </div>
+           
+       `
+        todosListEl.insertAdjacentHTML("afterbegin", Object)
+        updateInput(defaultItemData)
+
+    }
+})
+
+const focusItem = () => {  
+ defaultItem.style.border = "2px solid goldenrod"
+}
